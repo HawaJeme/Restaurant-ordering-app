531895510ca11e4f8ac750981c4b45050b9883fd
Hawa Jeme <kuuzey46@gmail.com>
2023-03-03 10:59:02 +0300

    final changes

--- index.js
+++ index.js
@@ -3,19 +3,19 @@ import { menuArray } from "./data.js";
 const checkoutForm = document.getElementById("checkout-form")
 const modal = document.getElementById('payment-checkout-modal')
 const orderField = document.getElementById('order-field')
+const allAddBtns = document.getElementsByClassName('fa-plus')
+document.getElementById('menu').innerHTML = renderMenu()
 
 document.addEventListener('click', function(e){
     if(e.target.dataset.add){
         orderField.style.display = 'flex';
         handleItemOrder(e.target.dataset.add)
-        totalPrice()
     } else if(e.target.dataset.remove){
         removeItem(e.target.dataset.remove)
     } else if (e.target.dataset.purchasebtn){
         cardPopup()
     }
 })
-render()
 
 let purchaseList = []
 let finishedOrder = false
@@ -24,16 +24,76 @@ function handleItemOrder(orderId){
     const targetItemObj = menuArray.filter(function(item){
         return item.id == orderId
     })[0]
-    const targetItemName = targetItemObj.name
-    const targetItemPrice = targetItemObj.price
     if(!finishedOrder){
     purchaseList.push({
-        Name : targetItemName,
-        Price : targetItemPrice
+        Name : targetItemObj.name,
+        Price : targetItemObj.price
     })
     renderOrdersHTML()
 }
 }
+
+function addItem(){
+    let newItem = ``
+    
+    purchaseList.forEach((item) => {
+    let findIndex = purchaseList.indexOf(item)
+
+        newItem += `
+        <div class="order-items">
+            <p>${item.Name}</p>
+            <p class="remove-btn" data-remove=${findIndex}>remove</p>
+            <p class="order-price">$${item.Price}</p>
+        </div>
+    `
+})
+return newItem
+}
+
+function removeItem(itemId) {
+    purchaseList.splice(itemId, 1)
+    if(purchaseList.length == 0){
+        orderField.style.display = 'none'
+    }
+    renderOrdersHTML()
+}
+
+function totalPrice(){
+
+    let totalPriceNum = 0
+
+    for( let i=0; i< purchaseList.length; i++){
+        totalPriceNum += (purchaseList[i].Price)
+    }
+    return totalPriceNum
+}
+
+function cardPopup(){
+    finishedOrder = true
+    modal.style.display = "block"
+    modal.classList = "fixed"
+    for(let i in allAddBtns){
+        allAddBtns[i].style.cursor = "not-allowed"
+    }
+}
+
+checkoutForm.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    const userName = document.getElementById('name').value
+    modal.style.display = 'none'
+    finishedOrder = false
+    purchaseList = []
+    let thanksMsg = ``
+    thanksMsg = `
+        <div class="thanks-modal">
+        <h3>Thanks, ${userName}! Your order is on its way!</h3>
+    </div>`
+
+    orderField.innerHTML = thanksMsg
+    for(let i in allAddBtns){
+        allAddBtns[i].style.cursor = "pointer"
+    }
+})
 
 function renderOrders(){
 
@@ -80,76 +140,6 @@ function renderMenu(){
     `
     })
     return MainHTML
-}
-
-function addItem(){
-    
-    let newItem = ``
-    
-    purchaseList.forEach((item) => {
-    let findIndex = purchaseList.indexOf(item)
-
-        newItem += `
-        <div class="order-items">
-            <p>${item.Name}</p>
-            <p class="remove-btn" data-remove=${findIndex}>remove</p>
-            <p class="order-price">$${item.Price}</p>
-        </div>
-    `
-})
-return newItem
-}
-
-function removeItem(itemId) {
-    purchaseList.splice(itemId, 1)
-    if(purchaseList.length == 0){
-        orderField.style.display = 'none'
-    }
-    renderOrdersHTML()
-}
-
-function totalPrice(){
-
-    let totalPriceNum = 0
-
-    for( let i=0; i< purchaseList.length; i++){
-        totalPriceNum += (purchaseList[i].Price)
-    }
-    return totalPriceNum
-}
-
-function cardPopup(){
-    finishedOrder = true
-    const purchaseBtn = document.getElementById('purchase-btn')
-    modal.style.display = "block"
-    modal.classList = "fixed"
-    purchaseBtn.style.cursor = "not-allowed"
-    const allAddBtns = document.getElementsByClassName('fa-plus')
-    for(let i in allAddBtns){
-        allAddBtns[i].style.cursor = "not-allowed"
-    }
-}
-
-checkoutForm.addEventListener('submit', (e)=>{
-    e.preventDefault()
-    const userName = document.getElementById('name').value
-    thanksMsg(userName)
-})
-
-function thanksMsg(name){
-    modal.style.display = 'none'
-
-    let thanksMsg = ``
-    thanksMsg = `
-    <div class="thanks-modal">
-    <h3>Thanks, ${name}! Your order is on its way!</h3>
-    </div>`
-
-    orderField.innerHTML = thanksMsg
-}
-
-function render(){
-    document.getElementById('menu').innerHTML = renderMenu()
 }
 
 function renderOrdersHTML(){
